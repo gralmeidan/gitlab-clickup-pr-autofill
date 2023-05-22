a5ebf599b63325198a243637b863c46af3aaaa60
gralmeidan <gralmeidan@gmail.com>
2023-05-22 07:18:15 -0400

    chore: run prettier format

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 const showLoading = () => {
-  document.createElement('div')
+  document.createElement('div');
 
-  const loading = document.createElement('div')
+  const loading = document.createElement('div');
 
-  loading.id = 'loading'
-  loading.innerHTML = '<p style="display: flex; justify-content: center;">Carregando...</p>'
+  loading.id = 'loading';
+  loading.innerHTML =
+    '<p style="display: flex; justify-content: center;">Carregando...</p>';
 
   Object.assign(loading.style, {
     position: 'fixed',
@@ -18,11 +19,10 @@ const showLoading = () => {
     backgroundColor: '#222222',
     border: 'solid 1px #EEEEEE',
     padding: '8px',
-  })
+  });
 
-
-  document.body.appendChild(loading)
-}
+  document.body.appendChild(loading);
+};
 
 const changePRTitle = () => {
   const [from, _] = [
@@ -35,15 +35,15 @@ const changePRTitle = () => {
 
   const prTitle = from!
     .replace(/(\S+)_(CU-\S+)/, '[ $2 ] $1')
-    .replace(/\-/g, (match, index) => index == 4 ? match : ' ');
+    .replace(/\-/g, (match, index) => (index == 4 ? match : ' '));
 
   titleInput.value = prTitle;
-}
+};
 
 const assignToMe = () => {
   const [assignee] = document.getElementsByClassName(
-    'dropdown-toggle-text',
-  ) as HTMLCollectionOf<HTMLSpanElement>
+    'dropdown-toggle-text'
+  ) as HTMLCollectionOf<HTMLSpanElement>;
 
   if (assignee.textContent === 'Unassigned') {
     const assignToMe = document.querySelector(
@@ -52,39 +52,40 @@ const assignToMe = () => {
 
     assignToMe.click();
   }
-}
+};
 
 const removeLoading = () => {
-  document.getElementById('loading')?.remove()
-}
+  document.getElementById('loading')?.remove();
+};
 
 const setReviewerToPedro = () => {
   const [_, reviewer] = document.getElementsByClassName(
-    'dropdown-toggle-text',
-  ) as HTMLCollectionOf<HTMLSpanElement>
+    'dropdown-toggle-text'
+  ) as HTMLCollectionOf<HTMLSpanElement>;
 
-  reviewer.click()
+  reviewer.click();
 
   const [__, reviewerSearchInput] = document.getElementsByClassName(
-    'dropdown-input-field',
-  ) as HTMLCollectionOf<HTMLInputElement>
+    'dropdown-input-field'
+  ) as HTMLCollectionOf<HTMLInputElement>;
 
-  reviewerSearchInput.value = 'Pedro Antonio'
+  reviewerSearchInput.value = 'Pedro Antonio';
 
   setTimeout(() => {
     const [__, reviewerResultsContainer] = document.getElementsByClassName(
-      'dropdown-content',
-    ) as HTMLCollectionOf<HTMLDivElement>
+      'dropdown-content'
+    ) as HTMLCollectionOf<HTMLDivElement>;
 
-    const [reviewerResults] = reviewerResultsContainer.getElementsByTagName('ul')
+    const [reviewerResults] =
+      reviewerResultsContainer.getElementsByTagName('ul');
 
-    const [firstResult] = reviewerResults.getElementsByTagName('li')
+    const [firstResult] = reviewerResults.getElementsByTagName('li');
 
-    firstResult.getElementsByTagName('a')[0].click()
+    firstResult.getElementsByTagName('a')[0].click();
 
-    removeLoading()
+    removeLoading();
   }, 2000);
-}
+};
 
 (() => {
   showLoading();
