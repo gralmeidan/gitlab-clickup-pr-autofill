e471c20a379d4a394813b5cd1917dd8d61fbb7a5
gralmeidan <gralmeidan@gmail.com>
2023-08-13 15:19:26 -0400

    refactor: checkbox component now controls input visibility

--- src/components/OptionInput.svelte
+++ src/components/OptionInput.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
   export let name: string;
   export let checked: boolean;
+  export let label: string;
 </script>
 
 <div class="my-2 text-base">
   <input type="checkbox" id="{name}-checkbox" {name} bind:checked />
-  <label for="{name}-checkbox"><slot /></label>
+  <label for="{name}-checkbox">{label}</label>
 </div>
+{#if checked}
+  <slot />
+{/if}
--- src/components/TitleConfig.svelte
+++ src/components/TitleConfig.svelte
@@ -2,6 +2,7 @@
   import Checkbox from './Checkbox.svelte';
   import type { RegexConfig } from 'src/types/configs.types';
   import RegexInput from './RegexInput.svelte';
+  import OptionInput from './OptionInput.svelte';
 
   export let configs: RegexConfig[];
   export let enabled: boolean;
@@ -22,24 +23,18 @@
   }
 </script>
 
-<section>
-  <Checkbox name="title-enabled" bind:checked={enabled}
-    >Autofill titles based on the source branch</Checkbox
+<OptionInput
+  name="title"
+  bind:checked={enabled}
+  label="Autofill titles based on source branch"
+>
+  {#each configs as { regex, replace }, i}
+    <RegexInput bind:regex bind:replace key={i} remove={() => removeField(i)} />
+  {/each}
+  <button
+    class="material-symbols-rounded text-4xl hover:text-cyan-500 transition-colors duration-200"
+    on:click={addField}
   >
-  {#if enabled}
-    {#each configs as { regex, replace }, i}
-      <RegexInput
-        bind:regex
-        bind:replace
-        key={i}
-        remove={() => removeField(i)}
-      />
-    {/each}
-    <button
-      class="material-symbols-rounded text-4xl hover:text-cyan-500 transition-colors duration-200"
-      on:click={addField}
-    >
-      add
-    </button>
-  {/if}
-</section>
+    add
+  </button>
+</OptionInput>
