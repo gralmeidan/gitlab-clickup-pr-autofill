77bd917199f0bea26da1c3a6a0bfe307572af37a
gralmeidan <gralmeidan@gmail.com>
2023-08-13 14:26:17 -0400

    feat: added title regex input and storage sync functionality

---
+++ src/components/RegexInput.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  export let regex: string;
+  export let replace: string;
+  export let key: number;
+</script>
+
+<div class="flex w-full">
+  <div class="flex-grow mr-3">
+    {#if key == 0}
+      <label for="{key}-title-regex-value">Regex</label>
+    {/if}
+    <input
+      type="text"
+      name="regex"
+      id="{key}-title-regex-value"
+      bind:value={regex}
+    />
+  </div>
+  <div>
+    {#if key == 0}
+      <label for="{key}-title-replace-value">Replace</label>
+    {/if}
+    <input
+      type="text"
+      name="replace"
+      id="{key}-title-replace-value"
+      bind:value={replace}
+    />
+  </div>
+</div>
+
+<style>
+  input {
+    font-family: 'Ubuntu Mono', monospace;
+    @apply text-base;
+  }
+
+  label {
+    @apply text-sm;
+  }
+</style>
---
+++ src/components/TitleConfig.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import type { RegexConfig } from 'src/types/configs.types';
+  import RegexInput from './RegexInput.svelte';
+
+  export let configs: RegexConfig[];
+
+  function addConfig() {
+    configs = [
+      ...configs,
+      {
+        regex: '',
+        replace: '',
+      },
+    ];
+  }
+</script>
+
+<section>
+  <h1>Title</h1>
+  {#each configs as { regex, replace }, i}
+    <RegexInput bind:regex bind:replace key={i} />
+  {/each}
+  <button
+    class="material-symbols-rounded text-4xl hover:text-cyan-500 transition-colors duration-200"
+    on:click={addConfig}
+  >
+    add
+  </button>
+</section>
